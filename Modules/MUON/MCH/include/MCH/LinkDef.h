a5163c2525c04142fd0ffbafba1ab377938759dd
Sebastien Perrin <sebastien.perrin@cern.ch>
2020-06-29 14:19:37 +0200

    Addition of a basic PhysicsCheck

--- Modules/MUON/MCH/include/MCH/LinkDef.h
+++ Modules/MUON/MCH/include/MCH/LinkDef.h
@@ -10,4 +10,5 @@
 #pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTaskDigits + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsTaskPreClusters + ;
 #pragma link C++ class o2::quality_control_modules::muonchambers::PedestalsCheck + ;
+#pragma link C++ class o2::quality_control_modules::muonchambers::PhysicsCheck + ;
 #endif
