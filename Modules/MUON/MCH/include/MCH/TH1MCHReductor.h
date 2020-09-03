67b2f14c6790370920baaec68b91e31674b3a414
Sebastien Perrin <sebastien.perrin@cern.ch>
2020-09-03 16:47:05 +0200

    Added treatement of all half-chambers using data from TH1 QC output

--- Modules/MUON/MCH/include/MCH/TH1MCHReductor.h
+++ Modules/MUON/MCH/include/MCH/TH1MCHReductor.h
@@ -39,12 +39,40 @@ class TH1MCHReductor : public quality_control::postprocessing::Reductor
    union {
      struct {
          Double_t occ819;
-         Double_t occ8O;
      } named;
-     Double_t array[2];
-   } occs;
+     Double_t indiv[1];
+   } indiv_occs;
+     union {
+       struct {
+           Double_t occ5I;
+           Double_t occ5O;
+           Double_t occ6I;
+           Double_t occ6O;
+           Double_t occ7I;
+           Double_t occ7O;
+           Double_t occ8I;
+           Double_t occ8O;
+           Double_t occ9I;
+           Double_t occ9O;
+           Double_t occ10I;
+           Double_t occ10O;
+       } named;
+         Double_t halfch[12];
+ } halfch_occs;
    Double_t entries;
  } mStats;
+    int detCH5I[9] = {500,501,502,503,504,514,515,516,517};
+    int detCH5O[9] = {505,506,507,508,509,510,511,512,513};
+    int detCH6I[9] = {600,601,602,603,604,614,615,616,617};
+    int detCH6O[9] = {605,606,607,608,609,610,611,612,613};
+    int detCH7I[13] = {700,701,702,703,704,705,706,720,721,722,723,724,725};
+    int detCH7O[13] = {707,708,709,710,711,712,713,714,715,716,717,718,719};
+    int detCH8I[13] = {800,801,802,803,804,805,806,820,821,822,823,824,825};
+    int detCH8O[13] = {807,808,809,810,811,812,813,814,815,816,817,818,819};
+    int detCH9I[13] = {900,901,902,903,904,905,906,920,921,922,923,924,925};
+    int detCH9O[13] = {907,908,909,910,911,912,913,914,915,916,917,918,919};
+    int detCH10I[13] = {1000,1001,1002,1003,1004,1005,1006,1020,1021,1022,1023,1024,1025};
+    int detCH10O[13] = {1007,1008,1009,1010,1011,1012,1013,1014,1015,1016,1017,1018,1019};
 };
 
 } // namespace o2::quality_control_modules::muonchambers
--- Modules/MUON/MCH/src/TH1MCHReductor.cxx
+++ Modules/MUON/MCH/src/TH1MCHReductor.cxx
@@ -29,7 +29,7 @@ void* TH1MCHReductor::getBranchAddress()
 
 const char* TH1MCHReductor::getBranchLeafList()
 {
-  return "occ819/D:occ8O:entries";
+  return "occ819/D:occ5I:occ5O:occ6I:occ6O:occ7I:occ7O:occ8I:occ8O:occ9I:occ9O:occ10I:occ10O:entries";
 }
 
 void TH1MCHReductor::update(TObject* obj)
@@ -38,16 +38,113 @@ void TH1MCHReductor::update(TObject* obj)
   if (histo) {
       double mean = 0;
     mStats.entries = histo->GetEntries();
-    mStats.occs.array[0] = histo->GetBinContent(819+1);
+    mStats.indiv_occs.indiv[0] = histo->GetBinContent(819+1);
       
-      for(int i=807; i<=819; i++){
+      //5I
+      for(int i : detCH5I){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 9;
+      mStats.halfch_occs.halfch[0] = mean;
+      mean = 0;
+      
+      //5O
+      for(int i : detCH5O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 9;
+      mStats.halfch_occs.halfch[1] = mean;
+      mean = 0;
+      
+      //6I
+      for(int i : detCH6I){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 9;
+      mStats.halfch_occs.halfch[2] = mean;
+      mean = 0;
+      
+      //6O
+      for(int i : detCH6O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 9;
+      mStats.halfch_occs.halfch[3] = mean;
+      mean = 0;
+      
+      //7I
+      for(int i : detCH7I){
           mean += histo->GetBinContent(i+1);
       }
       mean /= 13;
-      mStats.occs.array[1] = mean;
+      mStats.halfch_occs.halfch[4] = mean;
+      mean = 0;
+      
+      //7O
+      for(int i : detCH7O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[5] = mean;
+      mean = 0;
+      
+      //8I
+      for(int i : detCH8I){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[6] = mean;
+      mean = 0;
+      
+      //8O
+      for(int i : detCH8O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[7] = mean;
+      mean = 0;
+      
+      //9I
+      for(int i : detCH9I){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[8] = mean;
+      mean = 0;
+      
+      //9O
+      for(int i : detCH9O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[9] = mean;
+      mean = 0;
+      
+      //10I
+      for(int i : detCH10I){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[10] = mean;
+      mean = 0;
+      
+      //10O
+      for(int i : detCH10O){
+          mean += histo->GetBinContent(i+1);
+      }
+      mean /= 13;
+      mStats.halfch_occs.halfch[11] = mean;
+      mean = 0;
+      
+//      for(int i=807; i<=819; i++){
+//          mean += histo->GetBinContent(i+1);
+//      }
+//      mean /= 13;
+//      mStats.halfch_occs.halfch[7] = mean;
     
-      std::cout << "MeanOccupancy DE819 obtained from reductor " << mStats.occs.array[0] << std::endl;
-      std::cout << "MeanOccupancy Ch8O obtained from reductor " << mStats.occs.array[1] << std::endl;
+      std::cout << "MeanOccupancy DE819 obtained from reductor " << mStats.indiv_occs.indiv[0] << std::endl;
+      std::cout << "MeanOccupancy Ch5I obtained from reductor " << mStats.halfch_occs.halfch[0] << std::endl;
+      std::cout << "MeanOccupancy Ch8O obtained from reductor " << mStats.halfch_occs.halfch[7] << std::endl;
       
   }
 }
